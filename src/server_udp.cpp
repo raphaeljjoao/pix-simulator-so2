e59658a21bda84c00a4b43246c4042dc1ec07b92
João Raphael <raphaeljoao02@gmail.com>
2025-10-23 21:00:29 -0300

    Implement client handling and DISCOVERY_ACK

--- src/server_udp.cpp
+++ src/server_udp.cpp
@@ -9,9 +9,11 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <unordered_map>
 #include "network_structs.hpp"
 
 const int PORT = 4000;
+const int INITIAL_CLIENT_BALANCE = 100;
 
 int main(int argc, char *argv[])
 {
@@ -33,6 +35,7 @@ int main(int argc, char *argv[])
 	clilen = sizeof(struct sockaddr_in);
 
 	network_structs::Stats stats;
+	std::unordered_map<uint32_t, network_structs::Client> clients;
 	
 	auto now = std::chrono::system_clock::now();
 	auto time_t = std::chrono::system_clock::to_time_t(now);
@@ -47,13 +50,35 @@ int main(int argc, char *argv[])
 		/* receive from socket */
 		n = recvfrom(sockfd, &received_packet, sizeof(network_structs::Packet), 0, (struct sockaddr *) &cli_addr, &clilen);
 		if (n < 0) 
-			std::cout << "ERROR on recvfrom";
+			std::cout << "ERROR on recvfrom" << std::endl;
+		
 		std::cout << "Received a datagram: " << static_cast<int>(received_packet.type) << std::endl;
 		
-		/* send to socket */
-		n = sendto(sockfd, "Got your message\n", 17, 0,(struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
-		if (n  < 0) 
-			std::cout << "ERROR on sendto";
+		if (received_packet.type == network_structs::PacketType::DISCOVERY) {
+			uint32_t client_addr = cli_addr.sin_addr.s_addr;
+			
+			network_structs::Client new_client;
+			new_client.address = client_addr;
+			new_client.balance = INITIAL_CLIENT_BALANCE;
+			new_client.last_req = 0;
+			if (clients.find(client_addr) == clients.end()) {
+				clients[client_addr] = new_client;
+			}
+			
+			// Create and send DISCOVERY_ACK response
+			network_structs::Packet response_packet;
+			response_packet.type = network_structs::PacketType::DISCOVERY_ACK;
+			response_packet.sequence_number = received_packet.sequence_number;
+			
+			/* send response to socket */
+			n = sendto(sockfd, &response_packet, sizeof(network_structs::Packet), 0, (struct sockaddr *) &cli_addr, sizeof(struct sockaddr));
+			if (n < 0) 
+				std::cout << "ERROR on sendto" << std::endl;
+			else
+				std::cout << "Got your message" << std::endl;
+		} else {
+			std::cout << "Ignored packet" << std::endl;
+		}
 	}
 	
 	close(sockfd);
